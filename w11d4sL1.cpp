11b457728851ec96c8879878ab399c687adf1ae0
Yogesh Lakwal <110846547+yogeshlakwal3114@users.noreply.github.com>
2024-04-06 16:22:30 +0530

    second commit

--- w11d4sL1.cpp
+++ w11d4sL1.cpp
@@ -9,6 +9,7 @@ int n;
 int x[101], y[101];
 
 int dp[101][101];
+int back[101][101];
 int rec(int l, int r){
     // pruning
     if(l>r)return 0;
@@ -26,11 +27,27 @@ int rec(int l, int r){
     // Compute
     int ans = 1e9;
     for(int mid=l; mid<r; mid++){
-        ans = min(ans, rec(l, mid) + rec(mid+1, r) + x[l]*y[mid]*y[r]);
+        if(rec(l, mid) + rec(mid+1, r) + x[l]*y[mid]*y[r] < ans){
+            ans = rec(l, mid) + rec(mid+1, r) + x[l]*y[mid]*y[r];
+            back[l][r] = mid;
+        }
     }
 
     // Save and return
     return dp[l][r] = ans;
+}
+
+int opb[101];
+int clb[101];
+
+void generate(int l, int r){
+    if(l==r)return;
+
+    opb[l]++;
+    clb[r]++;
+    int mid = back[l][r];
+    generate(l, mid);
+    generate(mid+1, r);
 }
 
 void solve(){
@@ -41,6 +58,17 @@ void solve(){
 
     memset(dp, -1, sizeof(dp));
     cout<<rec(1,n)<<endl;
+
+    generate(1,n);
+    for(int i=1; i<=n; i++){
+        for(int x=0; x<opb[i]; x++){
+            cout<<"(";
+        }
+        cout<<" "<<i<<" ";
+        for(int x=0; x<clb[i]; x++){
+            cout<<")";
+        }
+    }
 }
 
 int main(){
